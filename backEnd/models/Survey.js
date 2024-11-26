3dabb7fdae6cf8434c639a92e3e0060df410a867
AlejaandroRubio <alejandro.rubio@alumnos.uneatlantico.es>
2024-11-26 11:37:36 +0100

    Implementación de Schemas y Server.js

---
+++ backEnd/models/Survey.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const surveySchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+    },
+    questions: [
+        {
+            tipo: {
+                type: String,
+                enum: ['text', 'opcionM', 'siNo'],
+                required: true
+            },
+            pregunta: {
+                type: String,
+                required: true
+            },
+            opciones: [String]
+        },
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Survey', surveySchema);
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 const PORT = process.env.PORT;
+dotenv.config();
+
+const authRoutes = require('./routes/auth');
+const surveyRoutes = require('./routes/survey');
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/survey', surveyRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API running');
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
